bab26d984933d7d13172f7d814b15fd92cc40363
emryslda <42739089+emryslda@users.noreply.github.com>
2019-04-06 13:23:11 +0200

    Create import_txt_hec_dss.py

---
+++ import_txt_hec_dss.py
@@ -0,0 +1,62 @@
+#INSERT THIS CODE IN HEC_DSSVue Script Editor.
+#Created on 06/04/2019
+#I will try to take this code up to date.
+
+from hec.script import *
+from hec.heclib.dss import *
+from hec.heclib.util import *
+from hec.io import *
+import java
+import os 
+
+try : 
+  try :
+    myDss = HecDss.open(PATH_TO_DSS") #IF IT DOESN'T EXIST IT CREATES IT
+    path=PATH_TO_TXTFILES
+    list_files=os.listdir(path)
+    for line in list_files: #LOOP OVER THE FILES
+      file=path+'/'+line; #READ A FILE
+      staz_name= ''.join([i for i in line if not i.isdigit()])
+      staz_name=staz_name.replace('.txt','') #DELETE SOME ISSUES
+      staz_name=staz_name.replace('_','')
+      tsc = TimeSeriesContainer()
+      #CHANGE THE 15MIN INTERVAL FOR YOUR NEEDS
+      tsc.fullName =  "/BASIN/"+staz_name+"/RAIN//15MIN/OBSERVATION/" #SETUP PATHNAME
+      station_file=open(file,'r')
+      #JUMP THE HEADERS
+      header1 = station_file.readline()
+      header2 = station_file.readline() 
+      header3 = station_file.readline()
+      header4 = station_file.readline()
+      data=[]
+      ora=[]
+      cum=[]
+      rain=[]
+      for line in station_file:
+         columns=line.split()
+         dates=columns[0][3:6]+columns[0][0:3]+columns[0][6:10]
+         data.append(dates)
+         ora.append(columns[1])
+         cum.append(columns[2])
+         precipitation=float(columns[3])
+         rain.append(precipitation)    
+      station_file.close()
+      tsc.interval = 15  #SETUP THE INTERVAL
+      times = []
+      start = HecTime(data[0],ora[0])
+      for value in rain :
+        times.append(start.value())
+        start.add(tsc.interval)
+      tsc.times = times
+      tsc.values = rain
+      tsc.numberValues = len(rain)
+      tsc.units = "mm"
+      tsc.type = "PRECIPITATION"
+      myDss.put(tsc)#HERE IT WILL UPDATE THE DSS FILE
+    
+  except Exception, e :
+    MessageBox.showError(' '.join(e.args), "Python Error")
+  except java.lang.Exception, e :
+    MessageBox.showError(e.getMessage(), "Error")
+finally :
+  myDss.close()
