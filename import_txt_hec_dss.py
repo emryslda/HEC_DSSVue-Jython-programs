d60e2b0846651682403b6e97f5fedc7bae1ab09e
emryslda <ludovico.diantonio@gmail.com>
2019-04-13 14:05:00 +0200

    lda

--- import_txt_hec_dss.py
+++ import_txt_hec_dss.py
@@ -7,12 +7,12 @@ from hec.heclib.dss import *
 from hec.heclib.util import *
 from hec.io import *
 import java
-import os 
-
-try : 
+import os
+#START PROGRAM
+try :
   try :
     #IF IT DOES NOT EXIST IT CREATES IT
-    myDss = HecDss.open(PATH_TO_DSS") 
+    myDss = HecDss.open(PATH_TO_DSS")
     path=PATH_TO_TXTFILES
     list_files=os.listdir(path)
     for line in list_files: #LOOP OVER THE FILES
@@ -29,7 +29,7 @@ try :
       station_file=open(file,'r')
       #JUMP THE HEADERS
       header1 = station_file.readline()
-      header2 = station_file.readline() 
+      header2 = station_file.readline()
       header3 = station_file.readline()
       header4 = station_file.readline()
       data=[]
@@ -43,10 +43,10 @@ try :
          ora.append(columns[1])
          cum.append(columns[2])
          precipitation=float(columns[3])
-         rain.append(precipitation)    
+         rain.append(precipitation)
       station_file.close()
-      #SETUP THE INTERVAL                  
-      tsc.interval = 15 
+      #SETUP THE INTERVAL
+      tsc.interval = 15
       times = []
       start = HecTime(data[0],ora[0])
       for value in rain :
@@ -58,7 +58,7 @@ try :
       tsc.units = "mm"
       tsc.type = "INST-CUM"
       myDss.put(tsc)#HERE IT WILL UPDATE THE DSS FILE
-    
+
   except Exception, e :
     MessageBox.showError(' '.join(e.args), "Python Error")
   except java.lang.Exception, e :
